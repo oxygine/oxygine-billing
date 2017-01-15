3421c1122ae1fb325153958233afb00ba2f7c5b2
dmuratshin <frankinshtein85@gmail.com>
2017-01-15 21:41:42 +0500

    min

--- example/HelloWorldBilling/src/example.cpp
+++ example/HelloWorldBilling/src/example.cpp
@@ -8,6 +8,7 @@ using namespace oxygine;
 
 string lastPurchasedItemToken;
 
+vector<string> items = {"org.oxygine.test"};
 
 class TestActor : public Test
 {
@@ -28,7 +29,7 @@ public:
 	{
         if (id == "purchase")
         {
-            billing::purchase("test", "123");
+            billing::purchase(items.front(), "123");
         }
         
         if (id == "get_purchases")
@@ -38,9 +39,6 @@ public:
 
         if (id == "get_details")
         {
-            std::vector<std::string> items;
-            items.push_back("test");
-
             billing::requestDetails(items);
         }
 
@@ -62,14 +60,19 @@ void example_init()
 	billing::init();
 
 #if 1
-    Json::Value items(Json::arrayValue);
+    Json::Value itemsObject(Json::arrayValue);
     
-    Json::Value item(Json::objectValue);
-    item["productId"] = "test";
-    item["price"] = "1 USD";
+    for (const auto &id:items)
+    {
+        Json::Value item(Json::objectValue);
+        item["productId"] = id;
+        item["price"] = "1 USD";
 
-    items.append(item);
-    billing::simulatorSetDetails(items);
+        itemsObject.append(item);
+    }
+
+
+    billing::simulatorSetDetails(itemsObject);
 #endif
 
 	billing::dispatcher()->addEventListener(billing::PurchasedEvent::EVENT, [](Event* e){
